19b18ffdfd380ea699d2a6ff60e0e85f9d989b0b
Miguel Rodríguez Herrero <rodriguezherreromiguel@gmail.com>
2022-03-11 11:32:40 +0100

    Rellenar e imprimir tabla fifo

--- src/GestrorProcesos/Ordenacion.java
+++ src/GestrorProcesos/Ordenacion.java
@@ -1,16 +1,25 @@
 package GestrorProcesos;
 
-import java.util.Map.Entry;
+import java.util.ArrayList;
 import java.util.TreeSet;
+
+import Administrador.ProcFifo;
 
 public class Ordenacion {
 
 	public static void fifo(TreeSet<Proceso> procesos) {
+		ArrayList<Proceso> procesoValoresLista = new ArrayList<>();
+		Proceso procFifoObj = new Proceso();
 		int instanteAnterior = 0;
 		int instanteComienzo = 0;
 		int tTotalAImprimir = 0;
+		int inicio = 0;
+		int fin = 0;
 		for (Proceso proceso : procesos) {
+
 			int instante = 0;
+			inicio = 0;
+			fin = 0;
 			int longitudProceso = proceso.gettEjecucion();
 			int instanteLlegada = proceso.getInstanteLlegada();
 			tTotalAImprimir += proceso.gettEjecucion();
@@ -18,17 +27,32 @@ public class Ordenacion {
 			while (instante < instanteAnterior) {
 				if (instante >= instanteLlegada) {
 					System.out.print(" E ");
+					inicio++;
+					fin++;
 				} else {
 					System.out.print(" - ");
+					inicio++;
+					fin++;
 				}
 				instante++;
 			}
 			instanteComienzo = instante;
 			for (int i = 0; i < longitudProceso; i++) {
 				System.out.print(" X ");
+				fin++;
 			}
 			System.out.println();
 			instanteAnterior = instanteComienzo + longitudProceso;
+
+			procFifoObj.setNombre(proceso.getNombre());
+			procFifoObj.setInstanteLlegada(proceso.getInstanteLlegada());
+			procFifoObj.settEjecucion(proceso.gettEjecucion());
+			procFifoObj.setInicio(inicio);
+			procFifoObj.setFin(fin);
+			procFifoObj.settTotal(fin - procFifoObj.getInstanteLlegada());
+			procFifoObj.settEspera(procFifoObj.gettTotal() - procFifoObj.gettEjecucion());
+			procFifoObj.setPenalizacion((float) procFifoObj.gettTotal() / procFifoObj.gettEjecucion());
+			procesoValoresLista.add(procFifoObj);
 		}
 		System.out.print("   ");
 		for (int i = 0; i < tTotalAImprimir + 2; i++) {
@@ -44,6 +68,10 @@ public class Ordenacion {
 		}
 		System.out.print("\n\n\n");
 		
-		
+		for (int i = 0; i < procesoValoresLista.size(); i++) {
+			System.out.println(procesoValoresLista.toString());
+			
+		}
+
 	}
 }
